6359cfc421e51d8e8572556be77fc027dc784e7a
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2024-04-01 20:00:05 +0300

    python 3.11, start codebase

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile, Request
+from pydantic import EmailStr
+from zipfile import ZipFile
 
 app = FastAPI()
 
@@ -6,3 +8,31 @@ app = FastAPI()
 @app.get("/hello-world")
 async def root():
     return {"message": "Hello World"}
+
+def try_utf8(data):
+    "Returns a Unicode object on success, or None on failure"
+    try:
+        return data.decode('utf-8')
+    except UnicodeDecodeError:
+       return None
+    
+@app.post("/")
+async def upload(
+    language: str = Form(),
+    user_email: EmailStr = Form(),
+    debias_zip: UploadFile = File()
+):
+    filename = debias_zip.filename
+    # texts = []
+    # with ZipFile(debias_zip.file) as zf:
+    #     for name in zf.namelist():
+    #         with zf.open(name) as f:
+    #             data = f.read()
+    #             udata = try_utf8(data)
+    #             if udata is None:
+    #                 return {"error": "File not encoded with utf-8"}
+    #             else:
+    #                 # Handle unicode data
+    #                 texts.append(udata)
+    
+    return {"filename": filename}
