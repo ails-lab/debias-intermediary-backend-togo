f551f6d883d35d4eb5e7148dc275aefbf079c101
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2024-04-02 13:49:24 +0300

    add parsing from zip

--- main.py
+++ main.py
@@ -16,23 +16,33 @@ def try_utf8(data):
     except UnicodeDecodeError:
        return None
     
+def parse_zip(uploaded_zip):
+    texts = []
+    filenames = []
+    with ZipFile(uploaded_zip.file) as zf:
+        for name in zf.namelist():
+            if zf.getinfo(name).is_dir():
+                continue
+            with zf.open(name) as f:
+                data = f.read()
+                udata = try_utf8(data)
+                if udata is None:
+                    return {"error": "File not encoded with utf-8"}
+                else:
+                    # Handle unicode data
+                    texts.append(udata)
+                    filenames.append(name)
+    return texts, filenames
+
 @app.post("/")
 async def upload(
     language: str = Form(),
     user_email: EmailStr = Form(),
     debias_zip: UploadFile = File()
 ):
-    filename = debias_zip.filename
-    # texts = []
-    # with ZipFile(debias_zip.file) as zf:
-    #     for name in zf.namelist():
-    #         with zf.open(name) as f:
-    #             data = f.read()
-    #             udata = try_utf8(data)
-    #             if udata is None:
-    #                 return {"error": "File not encoded with utf-8"}
-    #             else:
-    #                 # Handle unicode data
-    #                 texts.append(udata)
+    # filename = debias_zip.filename
+    texts, filenames = parse_zip(debias_zip)
     
-    return {"filename": filename}
+    dereference_dict = dict(zip(texts, filenames))
+    
+    return dereference_dict
